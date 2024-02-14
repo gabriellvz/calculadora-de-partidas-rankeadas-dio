c74122efc62de8d550d413fdacf2b94bc91e0084
gabriellvz <gabrielalvescruz0@gmail.com>
2024-02-13 23:07:26 -0300

    Projeto calculadora de partidas rankeadas

---
+++ index.js
@@ -0,0 +1,51 @@
+function calcularSaldoDeRanks(vitorias, derrotas) {
+
+    let saldoVitorias = vitorias - derrotas
+    let nivelDoRank = Math.floor(saldoVitorias/10)
+    
+
+        switch(nivelDoRank){
+
+            case 0:
+                console.log ("O herói tem o saldo de " + saldoVitorias + " e está no nível Ferro")
+                break
+
+            case 1:
+                console.log ("O herói tem o saldo de " + saldoVitorias + " e está no nível Bronze")
+                break
+            
+            case 2: 
+            case 3:
+            case 4:
+                console.log ("O herói tem o saldo de " + saldoVitorias + " e está no nível Prata")
+                break
+            
+            case 5: 
+            case 6:
+            case 7: 
+                console.log ("O herói tem o saldo de " + saldoVitorias + " e está no nível Ouro")
+                break
+
+            case 8: 
+                console.log ("O herói tem o saldo de " + saldoVitorias + " e está no nível Diamante")
+                break
+
+            case 9: 
+                console.log ("O herói tem o saldo de " + saldoVitorias + " e está no nível Lendário")
+                break
+
+            default: 
+                console.log ("O herói tem o saldo de " + saldoVitorias + " e está no nível Imortal")
+                break
+
+
+        }
+            
+    return saldoVitorias
+
+
+
+
+    
+}
+let resultado = calcularSaldoDeRanks (50 , 0)
